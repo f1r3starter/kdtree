8723bc376459d308d0c57535c5e554d9fab82be0
f1r3starter <andrey.filenochko@gmail.com>
2019-12-17 21:08:26 +0200

    add strict_types

--- src/Exception/InvalidPointProvided.php
+++ src/Exception/InvalidPointProvided.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace KDTree\Exceptions;
 
 class InvalidPointProvided extends \InvalidArgumentException
--- src/Exception/UnknownDimension.php
+++ src/Exception/UnknownDimension.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace KDTree\Exceptions;
 
 class UnknownDimension extends \InvalidArgumentException
